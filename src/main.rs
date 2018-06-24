dd7aab435c08281b1104de85770f04a0bc2f10c5
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-06-24 15:58:44 +0900

    Autoremove packages and only remove and install when there is something to be removed or uninstalled

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,13 @@ fn install_packages(packages: Vec<String>) {
     Command::new("pkg").arg("install").args(packages).status().unwrap();
 }
 
+fn autoremove_packages() {
+    Command::new("pkg").arg("autoremove").status().unwrap();
+}
+
 //TODO return for main, to avoid unwrapping
 fn main() {
     let output = Command::new("pkg").arg("prime-list").output().unwrap();
-    println!("{:?}", String::from_utf8_lossy(&output.stdout));
     let output = String::from_utf8_lossy(&output.stdout);
     let mut installed = HashSet::new();
     for line in output.lines() {
@@ -22,6 +25,7 @@ fn main() {
     }
 
     let mut content = String::new();
+    //TODO hardcoded path
     let mut file = File::open("/home/kirillvr/pkg_prime_list").unwrap();
     file.read_to_string(&mut content).unwrap();
 
@@ -30,10 +34,18 @@ fn main() {
         desired.insert(line);
     }
 
-    let to_be_installed = desired.difference(&installed);
-    println!("{:?}", to_be_installed);
-    let to_be_removed = installed.difference(&desired);
-    println!("{:?}", to_be_removed);
-    remove_packages(to_be_removed.map(|i| String::from(*i)).collect());
-    install_packages(to_be_installed.map(|i| String::from(*i)).collect());
+
+    let to_be_removed: Vec<String> = installed.difference(&desired).map(|i| String::from(*i)).collect();
+    if !to_be_removed.is_empty() {
+        println!("To be removed: {:?}", to_be_removed);
+        remove_packages(to_be_removed);
+    }
+
+    let to_be_installed: Vec<String> = desired.difference(&installed).map(|i| String::from(*i)).collect();
+    if !to_be_installed.is_empty() {
+        println!("To Be installed: {:?}", to_be_installed);
+        install_packages(to_be_installed);
+    }
+
+    autoremove_packages();
 }
